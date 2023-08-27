82d03876cd5b46394206dfa93f2066e99759644e
Aly Hegazy <alyyasser19@gmail.com>
2023-08-28 00:10:32 +0300

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import HeroSection from "@/components/HeroSection";
-import ServicesSection from "@/components/ServicesSection";
+import Loader from "@/components/Loader";
+import Pills3D from "@/components/Pills3D";
+import { useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
   return (
-    <main className="flex min-h-screen flex-col justify-between p-6 pt-5">
-      <div className="flex flex-col items-start">
-        <HeroSection />
-        <ServicesSection />
+    <main className="min-h-screen">
+      <div className="flex items-center flex-col justify-between p-6 pt-5 relative">
+        <Pills3D setLoading={setLoading} />
+        {loading ? (
+          <div className="h-48">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            <HeroSection />
+          </>
+        )}
       </div>
     </main>
   );
