fdd074c27a484e8afff88549dd97d3da323a2ae1
Aly Hegazy <alyyasser19@gmail.com>
2023-08-28 23:09:30 +0300

    Finalized main features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,29 @@ import ContactForm from "@/components/ContactForm";
 import HeroSection from "@/components/HeroSection";
 import Loader from "@/components/Loader";
 import Pills3D from "@/components/Pills3D";
+import Head from "next/head";
 import { useState } from "react";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   return (
-    <main className="min-h-screen">
-      <div className="flex items-center flex-col justify-between lg:p-6 sm:p-3 pt-5 relative">
-        {/* <Pills3D setLoading={setLoading} /> */}
-        {/* {loading ? (
+    <>
+      <main className="min-h-screen">
+        <div className="flex items-center flex-col justify-between lg:p-6 sm:p-3 pt-5 relative">
+          {/* <Pills3D setLoading={setLoading} /> */}
+          {/* {loading ? (
           <div className="h-48">
             <Loader />
           </div>
         ) : (
           <> */}
-        <>
-          <HeroSection />
-          <ContactForm />
-        </>
-        {/* )} */}
-      </div>
-    </main>
+          <>
+            <HeroSection />
+            <ContactForm />
+          </>
+          {/* )} */}
+        </div>
+      </main>
+    </>
   );
 }
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Logo() {
   return (
-    <h1 className={`${playfair.className} text-5xl text-transparent font-medium bg-white hover:bg-gradient-to-l from-green-500 to-green-300 bg-clip-text`}>
+    <h1 className={`${playfair.className} text-5xl text-transparent font-medium bg-white hover:bg-gradient-to-l from-green-500 to-green-300 bg-clip-text cursor-pointer`}>
       CCB.
     </h1>
   );
